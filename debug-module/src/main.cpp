eb1bba1cd0766dc8c75b69af3587d7819396257b
Farhat Samara <farhatsamara@gmail.com>
2025-12-01 18:11:19 -0600

    Added alarm test

--- debug-module/src/main.cpp
+++ debug-module/src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 
-// Simple Blink program
-// Circuit: digital pin #8 to anode (long leg) via resistor and GND to cathode (short leg)
+void runLedTest();
+void runAlarmTest();
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
   delay(2000);
   Serial.println("Arduino Uno Serial Log Enabled!"); 
@@ -13,9 +14,25 @@ void setup() {
   Serial.println("Pin 8 set as an output");
 }
 
-void loop() {
+void loop() 
+{
+  // runLedTest();
+  runAlarmTest();
+}
+
+void runLedTest()
+{
+  // Circuit: digital pin #8 to anode (long leg) via resistor and GND to cathode (short leg)
   digitalWrite(8, HIGH);
-  delay(500);
+  delay(1000);
   digitalWrite(8, LOW);
-  delay(500);
+  delay(1000);
+}
+
+void runAlarmTest()
+{
+  tone(8, 2000);
+  delay(200);
+  noTone(8);
+  delay(200);
 }
